4a6d28ffc9ba2efb2816fb11943fb61b27ffcc65
Ethan <421640644@qq.com>
2020-04-22 22:30:38 +0800

    modify mbg config

--- mall-tiny/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ mall-tiny/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -1,5 +1,6 @@
 package com.macro.mall.tiny;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallTinyApplication {
 
     public static void main(String[] args) {
+        LoggerFactory.getLogger(MallTinyApplication.class).info("http://localhost:8080/swagger-ui.html");
         SpringApplication.run(MallTinyApplication.class, args);
     }
 
